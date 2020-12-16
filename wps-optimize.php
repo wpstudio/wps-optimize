bcbe693650c0ea7cb2348dd32691bf2fe0e6122c
Benjamin <benjamin@wpstudio.com>
2020-12-16 10:20:41 -0500

    add additional gutenberg control for custom post types

--- wps-optimize.php
+++ wps-optimize.php
@@ -383,6 +383,7 @@ class OptWPSimize {
 
     private function disableGutenpoop(){
 	    add_filter('use_block_editor_for_post', '__return_false', 10);
+	    add_filter('use_block_editor_for_post_type', '__return_false', 10);
     } 
       
 }
